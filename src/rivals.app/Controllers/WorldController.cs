e985013faaa20f7518bdfaeae3c08c6d6ec19cb0
Adigun Palmer <apalmer@solsticebenefits.com>
2018-12-06 14:34:56 -0500

    Updated Login Behavoir

--- src/rivals.app/Controllers/WorldController.cs
+++ src/rivals.app/Controllers/WorldController.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using rivals.app.Models;
 
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace rivals.app.Controllers
 {
-    public class HomeController : Controller
+    [Authorize]
+    public class WorldController : Controller
     {
+        // GET: /<controller>/
         public IActionResult Index()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                return new RedirectResult("~/World");
-            }
-
-            var model = new HomeModel();
+        { 
+            var model = new WorldModel();
             return View(model);
         }
-        
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
